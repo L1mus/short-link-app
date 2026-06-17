c9d87e5ad35040d4ce35ef8b87424c24c4700d51
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 15:10:33 +0700

    feat: add method load godotenv and create dir for save image profile and implement env to method run

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 
+	_ "github.com/L1mus/short-link-app/server/docs"
+	"github.com/L1mus/short-link-app/server/internal/config"
+	"github.com/L1mus/short-link-app/server/internal/router"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+// @title           SHORT-LINK API
+// @version         1.0
+// @description     This is a backend project for a web application called SHORT-LINK .
+
+// @contact.name   Ali Mustadji
+// @contact.url    https://github.com/L1mus
+// @contact.email  limustadji@gmail.com
+
+// @license.name  MIT
+
+// @host      localhost:80/api
+// @BasePath  /
+
+// @securityDefinitions.apiKey  ApiKeyAuth
+// @in header
+// @name Authorization
+// @description Bearer token used for authorization
 func main() {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
@@ -10,4 +38,14 @@ func main() {
 		})
 	})
 	router.Run() // listens on 0.0.0.0:8080 by default
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env. \ncause: %s", err.Error())
+	}
+	if err := os.MkdirAll(filepath.Join("public", "img"), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create upload directory: %s", err.Error())
+	}
+	// inisialisasi
+	app := gin.Default()
+	// run
+	app.Run(fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT")))
 }
