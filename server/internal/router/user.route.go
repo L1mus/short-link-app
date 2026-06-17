dcf39fdbf5e9bca9f3cde6137e4a241f33fb1861
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 17:03:10 +0700

    feat: creat user route add methode GET and PATCH

---
+++ server/internal/router/user.route.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/L1mus/short-link-app/server/internal/controller"
+	"github.com/L1mus/short-link-app/server/internal/middleware"
+	"github.com/L1mus/short-link-app/server/internal/repository"
+	"github.com/L1mus/short-link-app/server/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func UserRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
+	userRouter := router.Group("/users")
+	userRouter.Use(middleware.VerifyToken, middleware.CheckBlacklist(rdb))
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository, rdb)
+	userController := controller.NewUserController(userService)
+
+	userRouter.GET("/profile", userController.GetUserProfile)
+	userRouter.PATCH("/profile", userController.UpdateProfile)
+}
