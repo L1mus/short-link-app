4d366360878fc453b1a985add5eeeebdae8a7d23
l1mus <ali.firefox@yahoo.co.id>
2026-06-18 05:46:04 +0700

    feat: add Model Links

--- server/internal/model/link.model.go
+++ server/internal/model/link.model.go
@@ -2,11 +2,17 @@ package model
 
 import "time"
 
+type Links struct {
+	ID          int        `db:"id"`
+	UserId      int        `db:"user_id"`
+	Slug        string     `db:"Slug"`
+	OriginalURL string     `db:"original_link"`
+	ClickCount  int        `db:"click_count"`
+	CreatedAt   time.Time  `db:"created_at"`
+	DeletedAt   *time.Time `db:"deleted_at"`
+}
 type GetAllLinks struct {
-	UserId      int       `db:"user_id"`
-	ShortLink   string    `db:"short_link"`
-	OriginalURL string    `db:"original_link"`
-	ClickCount  int       `db:"click_count"`
-	CreatedAt   time.Time `db:"created_at"`
-	TotalCount  int       `db:"total_count"`
+	Links
+	ShortLink  string `db:"short_link"`
+	TotalCount int    `db:"total_count"`
 }
