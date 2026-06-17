f70e12a79d83f3c715b96ca39df6a1ac9f25d1d1
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 17:29:40 +0700

    fix: bug on chema database NOT NULL at field deleted_at

--- server/schema/table.sql
+++ server/schema/table.sql
@@ -19,5 +19,21 @@ CREATE TABLE forgot_password
     expired_at TIMESTAMP           NOT NULL,
 
     CONSTRAINT fk_forgot_password_user FOREIGN KEY (user_id)
-    REFERENCES users (id) ON DELETE CASCADE
+        REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS links
+(
+    id           SERIAL PRIMARY KEY,
+    user_id      INT                NOT NULL,
+    original_url TEXT               NOT NULL,
+    slug         VARCHAR(50) UNIQUE NOT NULL,
+    created_at   TIMESTAMP          NOT NULL DEFAULT NOW(),
+    deleted_at   TIMESTAMP,
+
+    CONSTRAINT fk_links FOREIGN KEY (user_id)
+        REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_links_user_id ON links (user_id);
+CREATE UNIQUE INDEX idx_links_slug ON links (slug) WHERE deleted_at IS NULL;
