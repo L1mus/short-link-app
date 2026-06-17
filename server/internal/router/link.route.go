b759123a377db144df95e6a57af923f89e1fd82b
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 21:15:11 +0700

    feat: create route link method HTTP GET

---
+++ server/internal/router/link.route.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"github.com/L1mus/short-link-app/server/internal/controller"
+	"github.com/L1mus/short-link-app/server/internal/middleware"
+	"github.com/L1mus/short-link-app/server/internal/repository"
+	"github.com/L1mus/short-link-app/server/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+func LinkRouter(router *gin.Engine, db *pgxpool.Pool, rdb *redis.Client) {
+	linkRouter := router.Group("/links")
+	linkRouter.Use(middleware.VerifyToken, middleware.CheckBlacklist(rdb))
+
+	linkRepository := repository.NewLinkRepository(db)
+	linkService := service.NewLinkService(linkRepository)
+	linkController := controller.NewLinkController(linkService)
+
+	linkRouter.GET("/", linkController.GetAllLinks)
+}
