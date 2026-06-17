7299eb020837c13e4b1255c8e5ef5421ae18c0c2
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 16:59:19 +0700

    feat : errors massage

--- server/internal/appError/error.go
+++ server/internal/appError/error.go
@@ -7,6 +7,8 @@ var (
 	InvalidEmailFormat          = errors.New("invalid email format")
 	EmailOrPassWrong            = errors.New("wrong email or password")
 	UserNotFound                = errors.New("profile not found")
+	FileTooLarge                = errors.New("file size exceeds 2MB limit")
+	FileTypeNotAllowed          = errors.New("only jpg, png, and webp are allowed")
 	TokenDoesntExpired          = errors.New("token does not have expiration claim")
 	InvalidateSession           = errors.New("failed to invalidate session")
 	ForgotPasswordTokenNotFound = errors.New("token not found")
