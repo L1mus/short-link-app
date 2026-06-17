e4e1dedfaca0b26abbacf62c47bc046a232deb7f
l1mus <ali.firefox@yahoo.co.id>
2026-06-17 21:17:11 +0700

    chore(seeding): add random click count

--- server/db/seeds/seeding.sql
+++ server/db/seeds/seeding.sql
@@ -16,11 +16,12 @@ VALUES (1, 'token-reset-budi-12345', FALSE, NOW(), NOW() + INTERVAL '1 hour'),
        (2, 'token-reset-siti-67890', TRUE, NOW() - INTERVAL '2 days', NOW() - INTERVAL '1 day');
 
 -- 3. Insert 50 Data Links per User (Total 150 Data)
-INSERT INTO links (user_id, original_url, slug, created_at, deleted_at)
+INSERT INTO links (user_id, original_url, slug, click_count, created_at, deleted_at)
 SELECT
     u.id AS user_id,
     'https://sebuah-toko-online.com/kategori/promo-spesial/produk-unggulan-id-' || u.id || '-item-' || s.i AS original_url,
     SUBSTRING(MD5(u.id::TEXT || '-' || s.i::TEXT), 1, 6) AS slug,
+    FLOOR(RANDOM() * 20000)::INT AS click_count,
     NOW() - (s.i || ' hours')::INTERVAL AS created_at,
     NULL AS deleted_at
 FROM users u
