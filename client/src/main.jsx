733f3e8f854f0aa3b8606ff2d2c1c9dbae9465ea
l1mus <ali.firefox@yahoo.co.id>
2026-06-18 10:00:26 +0700

    feat: add toasty

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { store, persistor } from './redux/store.js';
 import './index.css';
 import App from './App.jsx';
@@ -11,7 +13,18 @@ createRoot(document.getElementById('root')).render(
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={3000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    pauseOnFocusLoss={false}
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
             </PersistGate>
         </Provider>
-    </StrictMode>,
+    </StrictMode>
 );
